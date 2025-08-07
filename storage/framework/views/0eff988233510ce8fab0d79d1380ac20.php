23a34c0e4515caf0c7558c1e0afd1eb6f5ae3908
white flower <114360170+dongBE2k5@users.noreply.github.com>
2025-08-08 02:32:17 +0700

    Update Database

---
+++ storage/framework/views/0eff988233510ce8fab0d79d1380ac20.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body> 
+    
+    
+    <h2>Xin chào <?php echo e($data['name']); ?></h2>
+    <p><?php echo e($data['message']); ?></p>
+</body>
+</html>
+<?php /**PATH E:\BE_NCHH\NCKH\resources\views/emails/notify.blade.php ENDPATH**/ ?>
